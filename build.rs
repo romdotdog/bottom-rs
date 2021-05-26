3ea2c195eee5bb2f7bc6490d35f13390196f0495
romdotdog <romdotdog@users.noreply.github.com>
2021-05-25 22:00:10 -0500

    calculate length at compile time

--- build.rs
+++ build.rs
@@ -5,72 +5,80 @@ use std::fs::File;
 use std::io::{BufWriter, Write};
 use std::path::Path;
 use std::{
-    collections::HashMap,
-    mem::{self, MaybeUninit},
+	collections::HashMap,
+	mem::{self, MaybeUninit},
 };
 
 lazy_static! {
 
-    static ref CHARACTER_VALUES: HashMap<u8, &'static str> = hashmap! {
-        200 => "🫂" ,
-        50 => "💖",
-        10 => "✨",
-        5 => "🥺",
-        1 => ",",
-        0 => "❤️",
-    };
-    static ref BYTE_TO_EMOJI: [String; 256] = {
-        // SAFETY: safe
-        let mut m: [MaybeUninit<String>; 256] = unsafe { MaybeUninit::uninit().assume_init() };
-        for i in 0..=255u8 {
-            m[i as usize] = MaybeUninit::new(byte_to_emoji(i));
-        }
-        unsafe { mem::transmute::<_, [String; 256]>(m) }
-    };
+	static ref CHARACTER_VALUES: HashMap<u8, &'static str> = hashmap! {
+		200 => "🫂" ,
+		50 => "💖",
+		10 => "✨",
+		5 => "🥺",
+		1 => ",",
+		0 => "❤️",
+	};
+	static ref BYTE_TO_EMOJI: [String; 256] = {
+		// SAFETY: safe
+		let mut m: [MaybeUninit<String>; 256] = unsafe { MaybeUninit::uninit().assume_init() };
+		for i in 0..=255u8 {
+			m[i as usize] = MaybeUninit::new(byte_to_emoji(i));
+		}
+		unsafe { mem::transmute::<_, [String; 256]>(m) }
+	};
 }
 
 fn byte_to_emoji(value: u8) -> String {
-    let mut buffer = String::new();
-    let mut value = value;
+	let mut buffer = String::new();
+	let mut value = value;
 
-    if value == 0 {
-        return CHARACTER_VALUES[&0].to_string();
-    }
+	if value == 0 {
+		return CHARACTER_VALUES[&0].to_string();
+	}
 
-    loop {
-        let (to_push, subtract_by) = {
-            if value >= 200 {
-                (CHARACTER_VALUES[&200], 200)
-            } else if value >= 50 {
-                (CHARACTER_VALUES[&50], 50)
-            } else if value >= 10 {
-                (CHARACTER_VALUES[&10], 10)
-            } else if value >= 5 {
-                (CHARACTER_VALUES[&5], 5)
-            } else if value >= 1 {
-                (CHARACTER_VALUES[&1], 1)
-            } else {
-                break;
-            }
-        };
+	loop {
+		let (to_push, subtract_by) = {
+			if value >= 200 {
+				(CHARACTER_VALUES[&200], 200)
+			} else if value >= 50 {
+				(CHARACTER_VALUES[&50], 50)
+			} else if value >= 10 {
+				(CHARACTER_VALUES[&10], 10)
+			} else if value >= 5 {
+				(CHARACTER_VALUES[&5], 5)
+			} else if value >= 1 {
+				(CHARACTER_VALUES[&1], 1)
+			} else {
+				break;
+			}
+		};
 
-        buffer.push_str(to_push);
-        value -= subtract_by;
-    }
+		buffer.push_str(to_push);
+		value -= subtract_by;
+	}
 
-    buffer.push_str("👉👈");
-    buffer
+	buffer.push_str("👉👈");
+	buffer
 }
 
 fn main() {
-    let path = Path::new(&env::var("OUT_DIR").unwrap()).join("maps.rs");
-    let mut file = BufWriter::new(File::create(&path).unwrap());
+	let path = Path::new(&env::var("OUT_DIR").unwrap()).join("maps.rs");
+	let mut file = BufWriter::new(File::create(&path).unwrap());
 
-    write!(&mut file, "static BYTE_TO_EMOJI: [&'static str; 256] = [").unwrap();
+	write!(&mut file, "static BYTE_TO_EMOJI: [&'static str; 256] = [").unwrap();
 
-    for emoji in BYTE_TO_EMOJI.iter() {
-        write!(&mut file, "\"{}\",", emoji).unwrap();
-    }
+	for emoji in BYTE_TO_EMOJI.iter() {
+		write!(&mut file, "\"{}\",", emoji).unwrap();
+	}
 
-    write!(&mut file, "];\n").unwrap();
+	write!(&mut file, "];\n").unwrap();
+
+	write!(&mut file, "static BYTE_TO_LEN: [u8; 256] = [").unwrap();
+
+	for emoji in BYTE_TO_EMOJI.iter() {
+		write!(&mut file, "{},", emoji.len()).unwrap();
+	}
+
+	write!(&mut file, "];\n").unwrap();
 }
--- src/bottom.rs
+++ src/bottom.rs
@@ -7,150 +7,151 @@ include!(concat!(env!("OUT_DIR"), "/maps.rs"));
 
 #[derive(Debug)]
 pub struct TranslationError {
-    pub why: String,
+	pub why: String,
 }
 
 impl fmt::Display for TranslationError {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "{}", self.why)
-    }
+	fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+		write!(f, "{}", self.why)
+	}
 }
 
 impl Error for TranslationError {}
 impl From<FromUtf8Error> for TranslationError {
-    fn from(error: FromUtf8Error) -> Self {
-        TranslationError {
-            why: format!("FromUtf8Error {}", error),
-        }
-    }
+	fn from(error: FromUtf8Error) -> Self {
+		TranslationError {
+			why: format!("FromUtf8Error {}", error),
+		}
+	}
 }
 
 pub fn encoded_len(input: &dyn AsRef<str>) -> usize {
-    let mut l: usize = 0;
-    for ch in input.as_ref().bytes() {
-        l += BYTE_TO_EMOJI[ch as usize].len();
-    }
-    l
+	let mut l: usize = 0;
+	for n in input.as_ref().bytes() {
+		l += BYTE_TO_LEN[n as usize] as usize;
+	}
+	l
 }
 
 pub fn encode_string(input: &dyn AsRef<str>) -> String {
-    let mut res = String::with_capacity(encoded_len(input));
-    for v in input.as_ref().bytes() {
-        res.push_str(BYTE_TO_EMOJI[v as usize])
-    }
-    res
+	let mut res = String::with_capacity(encoded_len(input));
+
+	for v in input.as_ref().bytes() {
+		res.push_str(BYTE_TO_EMOJI[v as usize])
+	}
+	res
 }
 
 pub fn decode_string(input: &dyn AsRef<str>) -> Result<String, TranslationError> {
-    let mut iter = input.as_ref().bytes();
-    let mut result: Vec<u8> = Vec::with_capacity(iter.clone().count());
+	let mut iter = input.as_ref().bytes();
+	let mut result: Vec<u8> = Vec::with_capacity(iter.clone().count());
 
-    'm: loop {
-        let mut sum: u8 = 0;
-        'b: loop {
-            let ch = iter.next();
-            if ch.is_none() {
-                break 'm;
-            }
+	'm: loop {
+		let mut sum: u8 = 0;
+		'b: loop {
+			let ch = iter.next();
+			if ch.is_none() {
+				break 'm;
+			}
 
-            let b = ch.unwrap();
-            match b {
-                240 => {
-                    iter.next(); // 159
-                    let b2 = iter.next().unwrap();
-                    match b2 {
-                        171 => {
-                            // people
-                            iter.next();
-                            sum += 200;
-                        }
-                        146 => {
-                            // heart
-                            iter.next();
-                            sum += 50;
-                        }
-                        165 => {
-                            // bottom
-                            iter.next();
-                            sum += 5;
-                        }
-                        145 => {
-                            // seperator
-                            iter.nth(4);
-                            break 'b;
-                        }
-                        _ => {
-                            return Err(TranslationError {
-                                why: format!("Attempt to decode byte {}", b2),
-                            });
-                        }
-                    }
-                }
-                226 => {
-                    let b2 = iter.next().unwrap();
-                    match b2 {
-                        156 => {
-                            // star
-                            iter.next();
-                            sum += 10;
-                        }
-                        157 => {
-                            // null heart
-                            iter.nth(3);
-                        }
-                        _ => {
-                            return Err(TranslationError {
-                                why: format!("Attempt to decode byte {}", b2),
-                            });
-                        }
-                    }
-                }
-                44 => {
-                    // comma
-                    sum += 1;
-                }
-                _ => {
-                    return Err(TranslationError {
-                        why: format!("Attempt to decode byte {}", b),
-                    });
-                }
-            }
-        }
-        result.push(sum)
-    }
+			let b = ch.unwrap();
+			match b {
+				240 => {
+					iter.next(); // 159
+					let b2 = iter.next().unwrap();
+					match b2 {
+						171 => {
+							// people
+							iter.next();
+							sum += 200;
+						}
+						146 => {
+							// heart
+							iter.next();
+							sum += 50;
+						}
+						165 => {
+							// bottom
+							iter.next();
+							sum += 5;
+						}
+						145 => {
+							// seperator
+							iter.nth(4);
+							break 'b;
+						}
+						_ => {
+							return Err(TranslationError {
+								why: format!("Attempt to decode byte {}", b2),
+							});
+						}
+					}
+				}
+				226 => {
+					let b2 = iter.next().unwrap();
+					match b2 {
+						156 => {
+							// star
+							iter.next();
+							sum += 10;
+						}
+						157 => {
+							// null heart
+							iter.nth(3);
+						}
+						_ => {
+							return Err(TranslationError {
+								why: format!("Attempt to decode byte {}", b2),
+							});
+						}
+					}
+				}
+				44 => {
+					// comma
+					sum += 1;
+				}
+				_ => {
+					return Err(TranslationError {
+						why: format!("Attempt to decode byte {}", b),
+					});
+				}
+			}
+		}
+		result.push(sum)
+	}
 
-    Ok(String::from_utf8(result)?)
+	Ok(String::from_utf8(result)?)
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+	use super::*;
 
-    #[test]
-    fn test_string_decode() {
-        // ~~Test that we haven't killed backwards-compat~~
-        // fuck backwards-compat
-        assert_eq!(
-            decode_string(&"💖✨✨✨,,,,👉👈💖💖,👉👈💖💖✨🥺👉👈💖💖✨🥺,👉👈").unwrap(),
-            "Test"
-        );
-    }
+	#[test]
+	fn test_string_decode() {
+		// ~~Test that we haven't killed backwards-compat~~
+		// fuck backwards-compat
+		assert_eq!(
+			decode_string(&"💖✨✨✨,,,,👉👈💖💖,👉👈💖💖✨🥺👉👈💖💖✨🥺,👉👈").unwrap(),
+			"Test"
+		);
+	}
 
-    #[test]
-    fn test_unicode_string_decode() {
-        assert_eq!(
-            decode_string(&"🫂✨✨✨✨👉👈💖💖💖🥺,,,,👉👈💖💖💖✨🥺👉👈💖💖💖✨✨✨🥺,👉👈")
-                .unwrap(),
-            "🥺",
-        );
-        assert_eq!(
-            decode_string(
-                &"🫂✨✨🥺,,👉👈💖💖✨✨🥺,,,,👉👈💖💖✨✨✨✨👉👈🫂✨✨🥺,,👉👈\
+	#[test]
+	fn test_unicode_string_decode() {
+		assert_eq!(
+			decode_string(&"🫂✨✨✨✨👉👈💖💖💖🥺,,,,👉👈💖💖💖✨🥺👉👈💖💖💖✨✨✨🥺,👉👈")
+				.unwrap(),
+			"🥺",
+		);
+		assert_eq!(
+			decode_string(
+				&"🫂✨✨🥺,,👉👈💖💖✨✨🥺,,,,👉👈💖💖✨✨✨✨👉👈🫂✨✨🥺,,👉👈\
             💖💖✨✨✨👉👈💖💖✨✨✨✨🥺,,👉👈🫂✨✨🥺,,👉👈💖💖✨✨🥺,,,,👉👈\
             💖💖💖✨✨🥺,👉👈🫂✨✨🥺,,👉👈💖💖✨✨✨👉👈💖💖✨✨✨✨👉👈"
-            )
-            .unwrap(),
-            "がんばれ",
-        );
-    }
+			)
+			.unwrap(),
+			"がんばれ",
+		);
+	}
 }
